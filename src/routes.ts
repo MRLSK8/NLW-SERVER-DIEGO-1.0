7aac90426db1dd4544b559502dc7bb06fa403b69
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-06-04 23:20:09 -0300

    Returning item_id

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ routes.get('/items', async (request, response) => {
 
   const serializedItems = items.map((item) => {
     return {
+      id: item.id,
       tittle: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
