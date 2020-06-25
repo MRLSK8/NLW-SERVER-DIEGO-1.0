5a2d8b4dbec9a9fe952360f5e9aff2a3bc2d73b0
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-06-25 12:14:47 -0300

    serialized points

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -57,21 +57,33 @@ class PointsController {
       return response.status(400).json({ Message: 'Point not found' });
     }
 
+    const serializedPoint = {
+      ...point,
+      image_url: `http://localhost:3333/uploads/${point.image}`,
+    };
+
     const items = await knex('items')
       .join('points_items', 'items.id', '=', 'points_items.item_id')
       .where('points_items.point_id', id);
 
-    return response.json({ point, items });
+    return response.json({ point: serializedPoint, items });
   }
 
   async showAll(request: Request, response: Response) {
-    const point = await knex('points').select('*');
+    const points = await knex('points').select('*');
 
-    if (!point) {
+    if (!points) {
       return response.status(400).json({ Message: 'Points not found' });
     }
 
-    return response.json({ point });
+    const serializedPoints = points.map((point) => {
+      return {
+        ...point,
+        image_url: `http://localhost:3333/uploads/${point.image}`,
+      };
+    });
+
+    return response.json({ point: serializedPoints });
   }
 
   async index(request: Request, response: Response) {
@@ -93,7 +105,14 @@ class PointsController {
       return response.status(400).json({ Message: 'No points found' });
     }
 
-    return response.json(points);
+    const serializedPoints = points.map((point) => {
+      return {
+        ...point,
+        image_url: `http://localhost:3333/uploads/${point.image}`,
+      };
+    });
+
+    return response.json({ point: serializedPoints });
   }
 }
 
